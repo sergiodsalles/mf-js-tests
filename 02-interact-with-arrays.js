fa4a2a2b513842c749989b10d41a456cb30eb99e
Sérgio Salles <sergiodesalles@gmail.com>
2022-12-20 20:50:32 -0300

    Interact with array exercise

---
+++ 02-interact-with-arrays.js
@@ -0,0 +1,23 @@
+/**
+ * Given two lists representing a shopping list and the prices respectively.
+ * Print on the console a friendly message with each item and the repective price.
+ * Obs: The item and its price is on the same position of the list (array).
+ *
+ * Example of console output:
+ *
+ * Your shopping list is:
+ * Acer Laptop - $ 2900
+ * Pacco Bottle - $ 190.60
+ * ...
+ *
+ */
+
+const items = [
+  "Acer Laptop",
+  "Pacco Bottle",
+  "Microsoft Mouse",
+  "Alexa Echo Dot 4",
+];
+const prices = [2900, 190.6, 59.9, 399.9];
+
+// Write your code here.
