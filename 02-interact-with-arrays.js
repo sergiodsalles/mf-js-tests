f9e42a88767fea6db5b992f9d84296e371345ec9
Michele Freitas <michele@pop-os.localdomain>
2022-12-20 21:06:21 -0300

    Interacting arrays

--- 02-interact-with-arrays.js
+++ 02-interact-with-arrays.js
@@ -1,6 +1,6 @@
 /**
  * Given two lists representing a shopping list and the prices respectively.
- * Print on the console a friendly message with each item and the repective price.
+ * Print on the console a friendly message with each item and the respective price.
  * Obs: The item and its price is on the same position of the list (array).
  *
  * Example of console output:
@@ -21,3 +21,9 @@ const items = [
 const prices = [2900, 190.6, 59.9, 399.9];
 
 // Write your code here.
+let descricao = "";
+console.log("Your shopping list is ");
+for (let i = 0; i < 4; i++) {
+  descricao = `${items[i]} - ${prices[i]}`;
+  console.log(descricao);
+}
